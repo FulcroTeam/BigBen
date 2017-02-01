aa4c54fc9d4a805845a75036c84593a015780a52
Sergio Parisi <sergioparisi98@hotmail.it>
2017-02-01 21:29:24 +0100

    1 feb 17 bis

--- static/js/script.js
+++ static/js/script.js
@@ -12,15 +12,4 @@ $(document).ready(function() {
       $('#kitchen_light').prop('checked', data['on']);
     });
   });
-  $('#condizionatore').click(function(e) {
-    var request_data = {
-      'command' : 'toggle',
-      'pin' : 3,
-      'value' : 0
-    };
-    $.post('http://localhost:8080/ajax', request_data, function(data) {
-      console.log(data);
-      $('#condizionatore').prop('checked', data['on']);
-    });
-  });
 });
